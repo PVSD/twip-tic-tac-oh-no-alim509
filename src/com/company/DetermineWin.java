e54e1d5729113b14355d92368ed0925a19ee09c9
al5091 <angelovinolim@gmail.com>
2018-12-17 11:21:38 -0500

    wack

---
+++ src/com/company/DetermineWin.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by al5091 on 12/17/18.
+ */
+public class DetermineWin {
+
+    public boolean DetermineWin(String[][] determine) {
+
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        String[][] board = new String[3][3];
+        int turn;
+        String position;
+
+        System.out.println("Let's play a game of X's and O'x. Type 1 if you would like to go first and 2 if you would like to play second.");
+        turn = sc.nextInt();
+
+        if (turn == 1)
+        {
+            System.out.println("Okay! Where would you like to place an X? (hint: use positions; (0, 0) would be top-left, (1, 1) would be the middle, etc.");
+        }
+        else if (turn == 2)
+        {
+            System.out.println("Okay! Here is my first move.");
+        }
+        else
+        {
+            System.out.println("That is not a 1 or a 2, so I will just go first.");
+            System.out.println("Okay! Here is my first move.");
+            turn = 2;
+        }
+
+        if (turn == 1)
+        {
+            while ()
+        }
     }
 }
